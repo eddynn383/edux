78ed347be6d6ffc23a3fd987ad837c54ab229329
EDC <85519028+eddynn383@users.noreply.github.com>
2023-04-30 21:15:38 +0300

    change mobile style

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -32,29 +32,7 @@ const AuthLayout = ({ children, device }:any) => {
         </div>
     )
 
-    const mobileLayout = (
-        <div className={sx.auth}>
-            <div className={sx.top}>
-                <div className={sx.inner}>
-                    <div className={sx.wallpaper}>
-                        <Image className={sx.image} src={imageUrl} alt="Auth Wallpaper" />
-                    </div>
-                </div>
-            </div>
-            <div className={sx.bottom}>
-                <div className={sx.inner}>
-                    <div className={sx.logo}>
-                        <Image className={sx.volvo} src={logo} alt="Volvo"/>
-                    </div>
-                    <div className={sx.form}>
-                        {children}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-
-    return device === "mobile" ? mobileLayout : desktopLayout
+    return desktopLayout
 }
 
 export default AuthLayout
